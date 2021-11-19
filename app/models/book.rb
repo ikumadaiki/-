b8c6409bfe7c0a4e590dc3e4fbc316595774f5f2
EC2 Default User <ec2-user@ip-172-31-27-178.ec2.internal>
2021-11-19 08:57:00 +0000

    7-a

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,14 +3,14 @@ class Book < ApplicationRecord
 	has_many :favorites, dependent: :destroy
 	has_many :book_comments, dependent: :destroy
 	has_many :favorited_users, through: :favorites, source: :user #いいね数で順番を変えるための
-	
+
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
 	end
-	
+
 	def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
@@ -21,5 +21,9 @@ class Book < ApplicationRecord
     else
       Book.where('title LIKE ?', '%'+content+'%')
     end
+  end
+
+  def self.last_week # メソッド名は何でも良いです
+    Book.joins(:favorites).where(favorites: { created_at:0.days.ago.prev_week..0.days.ago.prev_week(:sunday)}).group(:id).order("count(*) desc")
   end
 end
